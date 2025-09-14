00ae1163d1381660d05d300ae0b842be2bd86763
Rachel Phan <py.rachel03@gmail.com>
2025-09-14 11:20:39 -0400

    added home, reorganized files
    Created and edited the Home Screen, moved all screens to screens Folder

---
+++ screens/Home.js
@@ -0,0 +1,7 @@
+import { StyleSheet, Text, View, ImageBackground, ScrollView, Pressable } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router';
+
+export default function Home() {
+
+}
