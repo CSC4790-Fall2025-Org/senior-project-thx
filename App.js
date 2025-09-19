149e9298ebf4fda453bde26cdf7490753ce39663
Allyssa <166740534+allyssapanganiban@users.noreply.github.com>
2025-09-19 13:25:59 -0400

    Reset index.js, App.js, package.json, package-lock.json to match develop

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import SavedServices from './screens/SavedServices';
-import Profile from './screens/Profile';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppStack } from './navigation/Stack';
+import { LinearGradient } from 'expo-linear-gradient';
+console.log("App.js loaded");
 
 export default function App() {
-  // return <SavedServices />;
-  return <Profile />;
+  return (
+   <NavigationContainer>
+      <AppStack />
+   </NavigationContainer>
+  );
 }
+
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import Saved from './screens/SavedServices';
-import Profile from './screens/Profile';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
