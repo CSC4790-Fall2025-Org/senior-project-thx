5a0dc857fa42d50e1f75d7d3d8579845ecdb27a7
a-loyson <145863082+a-loyson@users.noreply.github.com>
2025-11-27 00:52:38 -0500

    corrected access token lifetime

--- thx-django/thx_django/settings.py
+++ thx-django/thx_django/settings.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3),  
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),    
     'ROTATE_REFRESH_TOKENS': True,                 
     'BLACKLIST_AFTER_ROTATION': True,             
